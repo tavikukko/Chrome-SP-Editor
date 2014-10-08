235a5783ba897337d3624eac839678b6d4147661
tavikukko <tomi.tavela@elisanet.fi>
2014-10-08 15:29:19 +0300

    Update event.js

--- event.js
+++ event.js
@@ -1,25 +1,44 @@
 chrome.devtools.inspectedWindow.onResourceContentCommitted.addListener(function(event, content) {
-alert(event.url);
-alert(content);
+	var updateFile = function updateFile() {
 
-var script = "var clientContext;" +
-"var website;" +
+	    var clientContext = new SP.ClientContext.get_current();
+	    
+	    var relUrl = url.replace(_spPageContextInfo.siteAbsoluteUrl,'');
+	    var file = clientContext.get_site().get_rootWeb().getFileByServerRelativeUrl(url); 
+	    var listItem = file.get_listItemAllFields();
+		var lista = listItem.get_parentList();
+	                
+		var fileCreateInfo = new SP.FileCreationInformation();
+	        fileCreateInfo.set_url("REPLACE-FILE-URL");
+	        fileCreateInfo.set_content(new SP.Base64EncodedByteArray());
+	        fileCreateInfo.set_overwrite(true);
+	        fileContent = "REPLACE-CONTENT";
+		var unescapedFileContent = unescape(fileContent);
 
-"SP.SOD.executeFunc('sp.js', 'SP.ClientContext', sharePointReady);" +
+	        for (var i = 0; i < unescapedFileContent.length; i++) {
+	    
+	            fileCreateInfo.get_content().append(unescapedFileContent.charCodeAt(i));
+	        }
+	    
+	     var existingFile = lista.get_rootFolder().get_files().add(fileCreateInfo);
+	                
+		clientContext.load(existingFile);
+		clientContext.executeQueryAsync(onRequestSucceeded, onRequestFailed);        
+	};
 
-"function sharePointReady() {" +
-"    clientContext = SP.ClientContext.get_current();" +
-"    website = clientContext.get_web();" +
+	var onRequestSucceeded = function onRequestSucceeded() {
+		alert('Done');
+	};
 
-"    clientContext.load(website);" +
-"    clientContext.executeQueryAsync(onRequestSucceeded, onRequestFailed);" +
-"}"+
-"function onRequestSucceeded() {" +
-"    alert('URL of the website: ' + website.get_url());" +
-"}"+
-"function onRequestFailed(sender, args) {" +
-"    alert('Error: ' + args.get_message());" +
-"}";
+	var onRequestFailed = function onRequestFailed(sender, args) {
+	    alert(args.get_message());
+	};
 
-      chrome.devtools.inspectedWindow.eval(script);
+	var script = updateFile + ' ' + onRequestSucceeded + ' ' + onRequestFailed;
+	script = script.replace("REPLACE-FILE-URL", event.url);
+	script = script.replace("REPLACE-CONTENT", escape(content));
+	script = script + " updateFile();";
+	//alert(script.substring(script.length - 400));
+
+	chrome.devtools.inspectedWindow.eval(script);
 });
