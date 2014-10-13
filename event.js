f28cf1e11744e3c27d64010cccdb239575044080
Tomi Tavela <tomi.tavela@elisanet.fi>
2014-10-13 15:44:42 +0300

    Update event.js
    added support for files on subsites.

--- event.js
+++ event.js
@@ -20,33 +20,57 @@ chrome.devtools.inspectedWindow.onResourceContentCommitted.addListener(function(
 	port.onMessage.addListener(function () {
 
 		var updateFile = function updateFile() {
+			
 			SP.SOD.executeFunc('sp.js', 'SP.ClientContext', function () {
-				var fileAbsUrl = "REPLACE-FILE-URL";
-				var siteAbsoluteUrl = _spPageContextInfo.siteAbsoluteUrl;
-				var siteServerRelativeUrl = _spPageContextInfo.siteServerRelativeUrl;
+				SP.SOD.executeFunc('sp.requestexecutor.js', 'SP.RequestExecutor', function () {
+					var fileAbsUrl = "REPLACE-FILE-URL";
+					var siteAbsoluteUrl = _spPageContextInfo.siteAbsoluteUrl;
+					var siteServerRelativeUrl = _spPageContextInfo.siteServerRelativeUrl;
 					if(siteServerRelativeUrl.length == 1) siteAbsoluteUrl += siteServerRelativeUrl;
-				var fileRelUrl = fileAbsUrl.replace(siteAbsoluteUrl.substring(0, siteAbsoluteUrl.lastIndexOf(siteServerRelativeUrl)),'');
+					var fileRelUrl = fileAbsUrl.replace(siteAbsoluteUrl.substring(0, siteAbsoluteUrl.lastIndexOf(siteServerRelativeUrl)),"");
+					var documentPath = fileAbsUrl.substring(0, fileAbsUrl.lastIndexOf("/"));
 
-				var fileContent = "REPLACE-CONTENT";
-				var unescapedFileContent = unescape(fileContent);
+					var fileContent = "REPLACE-CONTENT";
+					var unescapedFileContent = unescape(fileContent);
 
-				var clientContext = new SP.ClientContext.get_current();
+					var executor = new SP.RequestExecutor(documentPath);
+					
+					executor.executeAsync({
+						url: documentPath + "/_api/contextinfo",
+						method: "POST",
+						headers: { "Accept": "application/json; odata=verbose" },
+						success: function (data) {
+							if (data.body) {
+								var body = JSON.parse(data.body);
+								var webUrl = body.d.GetContextWebInformation.WebFullUrl;
 
-				var file = clientContext.get_site().get_rootWeb().getFileByServerRelativeUrl(fileRelUrl);
+								var fileContent = "REPLACE-CONTENT";
+								var unescapedFileContent = unescape(fileContent);
 
-				var fileCreateInfo = new SP.FileCreationInformation();
-				fileCreateInfo.set_content(new SP.Base64EncodedByteArray());
+								var clientContext = new SP.ClientContext(webUrl);
 
-				for (var i = 0; i < unescapedFileContent.length; i++) {
-					fileCreateInfo.get_content().append(unescapedFileContent.charCodeAt(i));
-				}
+								var file = clientContext.get_web().getFileByServerRelativeUrl(fileRelUrl);
 
-				file.checkOut();
-				file.saveBinary(fileCreateInfo);
-				file.checkIn();
-				file.publish();
-				clientContext.load(file);
-				clientContext.executeQueryAsync(onRequestSucceeded, onRequestFailed);
+								var fileCreateInfo = new SP.FileCreationInformation();
+								fileCreateInfo.set_content(new SP.Base64EncodedByteArray());
+
+								for (var i = 0; i < unescapedFileContent.length; i++) {
+									fileCreateInfo.get_content().append(unescapedFileContent.charCodeAt(i));
+								}
+
+								file.checkOut();
+								file.saveBinary(fileCreateInfo);
+								file.checkIn();
+								file.publish();
+								clientContext.load(file);
+								clientContext.executeQueryAsync(onRequestSucceeded, onRequestFailed);
+							}
+						},
+						error: function (err) {
+							alert(JSON.stringify(err));
+						}
+					});
+				});
 			});
 		};
 
