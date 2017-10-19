5058bb1e705f509357fc2eda36956afe1e541e64
ytterstrom <thomas.ytterstrom@gmail.com>
2017-10-19 12:10:03 +0200

    Revert "Added snippet for request headers (SP2013)"
    This reverts commit d2884854f7fce599982deca85b0227c2afed54dc.

--- app/snippets.js
+++ app/snippets.js
@@ -8,7 +8,7 @@ function createDependencyProposals() {
       insertText: {
         value: [
           "pnp.sp.web.get().then(${1:web} => {",
-          "\tconsole.log(${1})",
+            "\tconsole.log(${1})",
           "});"
         ].join('\n')
       }
@@ -20,7 +20,7 @@ function createDependencyProposals() {
       insertText: {
         value: [
           "pnp.sp.web.select(\"${1:Title}\").get().then(${2:web} => {",
-          "\tconsole.log(${2}.${1})",
+            "\tconsole.log(${2}.${1})",
           "});"
         ].join('\n')
       }
@@ -32,7 +32,7 @@ function createDependencyProposals() {
       insertText: {
         value: [
           "pnp.sp.web.select(\"AllProperties\").expand(\"AllProperties\").get().then(${1:web} => {",
-          "\tconsole.log(${1})",
+            "\tconsole.log(${1})",
           "});"
         ].join('\n')
       }
@@ -46,7 +46,7 @@ function createDependencyProposals() {
           "import { Web } from \"pnp\";",
           "let web = new Web(\"https://${1:tenant}.sharepoint.com\");",
           "web.get().then(${2:w} => {",
-          "\tconsole.log(${2})",
+            "\tconsole.log(${2})",
           "});",
         ].join('\n')
       }
@@ -60,7 +60,7 @@ function createDependencyProposals() {
           "import { Web } from \"pnp\";",
           "let web = new Web(\"https://${1:tenant}.sharepoint.com/mysubsite\");",
           "web.delete().then(${2:w} => {",
-          "\tconsole.log(${2})",
+            "\tconsole.log(${2})",
           "});",
         ].join('\n')
       }
@@ -72,10 +72,10 @@ function createDependencyProposals() {
       insertText: {
         value: [
           "pnp.sp.web.update({",
-          "\t${1:Title}: \"New Title\",",
-          "\t${2:Description}: \"My new description\",",
+            "\t${1:Title}: \"New Title\",",
+            "\t${2:Description}: \"My new description\",",
           "}).then(${3:web} => {",
-          "\tconsole.log(${3})",
+            "\tconsole.log(${3})",
           "});",
         ].join('\n')
       }
@@ -93,51 +93,35 @@ function createDependencyProposals() {
           "// Gets the accesstoken and fetches the desired url",
           "// Scope: Group.ReadWrite.All, Reports.Read.All",
           "async function getGroupData(url: string, method?: string) {",
-          "\tlet rspCtx = await fetch(`${(window as any)._spPageContextInfo.webAbsoluteUrl}/_api/contextinfo`, {",
-          "\t\tmethod: 'POST',",
-          "\t\tcredentials: 'include',",
-          "\t\theaders: new Headers({",
-          "\t\t\t'Accept': 'application/json; odata=verbose'",
-          "\t\t})",
-          "\t});\n",
-          "\tlet data = await rspCtx.json();",
-          "\tlet payload = { 'resource': 'https://graph.microsoft.com' };",
-          "\tlet token = await fetch(`${(window as any)._spPageContextInfo.webAbsoluteUrl}/_api/SP.OAuth.Token/Acquire`, {",
-          "\t\tmethod: 'POST',",
-          "\t\tcredentials: 'include',",
-          "\t\tbody: JSON.stringify(payload),",
-          "\t\theaders: new Headers({",
-          "\t\t\t'Accept': 'application/json; odata=verbose',",
-          "\t\t\t'Content-Type': 'application/json;odata=nometadata',",
-          "\t\t\t'X-RequestDigest': data.d.GetContextWebInformation.FormDigestValue",
-          "\t\t})",
-          "\t});\n",
-          "\tlet rspToken = await token.json();",
-          "\tlet reqData = await fetch(url, {",
-          "\t\tmethod: method ? method : 'GET',",
-          "\t\theaders: new Headers({",
-          "\t\t\t'Accept': 'application/json',",
-          "\t\t\t'Authorization': 'Bearer ' + rspToken.d.access_token",
-          "\t\t})",
-          "\t});\n",
-          "\treturn reqData.json();",
+            "\tlet rspCtx = await fetch(`${(window as any)._spPageContextInfo.webAbsoluteUrl}/_api/contextinfo`, {",
+              "\t\tmethod: 'POST',",
+              "\t\tcredentials: 'include',",
+              "\t\theaders: new Headers({",
+                "\t\t\t'Accept': 'application/json; odata=verbose'",
+              "\t\t})",
+            "\t});\n",
+            "\tlet data = await rspCtx.json();",
+            "\tlet payload = { 'resource': 'https://graph.microsoft.com' };",
+            "\tlet token = await fetch(`${(window as any)._spPageContextInfo.webAbsoluteUrl}/_api/SP.OAuth.Token/Acquire`, {",
+              "\t\tmethod: 'POST',",
+              "\t\tcredentials: 'include',",
+              "\t\tbody: JSON.stringify(payload),",
+              "\t\theaders: new Headers({",
+                "\t\t\t'Accept': 'application/json; odata=verbose',",
+                "\t\t\t'Content-Type': 'application/json;odata=nometadata',",
+                "\t\t\t'X-RequestDigest': data.d.GetContextWebInformation.FormDigestValue",
+              "\t\t})",
+            "\t});\n",
+            "\tlet rspToken = await token.json();",
+            "\tlet reqData = await fetch(url, {",
+              "\t\tmethod: method ? method : 'GET',",
+              "\t\theaders: new Headers({",
+                "\t\t\t'Accept': 'application/json',",
+                "\t\t\t'Authorization': 'Bearer ' + rspToken.d.access_token",
+              "\t\t})",
+            "\t});\n",
+            "\treturn reqData.json();",
           "}",
-        ].join('\n')
-      }
-    },
-    {
-      label: 'snip-headers',
-      kind: monaco.languages.CompletionItemKind.Snippet,
-      documentation: "Include request headers for SP2013",
-      insertText: {
-        value: [
-          "pnp.setup({",
-          "\tsp:{",
-          "\t\theaders: {",
-          "\t\t\t\"Accept\": \"application/json; odata=verbose\"",
-          "\t\t}",
-          "\t}",
-          "});"
         ].join('\n')
       }
     },
