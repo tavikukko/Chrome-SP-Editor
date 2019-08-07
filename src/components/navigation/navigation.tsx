e4a3f520c1f022584bbf2cc81046dbf156a21fbb
Tomi Tavela <tomi@Tomis-MacBook-Pro.local>
2019-08-07 12:41:05 +0300

    navigation controll with state to set selectediKey

--- src/components/navigation/navigation.tsx
+++ src/components/navigation/navigation.tsx
@@ -1,34 +1,61 @@
-import React from 'react';
-import { withRouter } from 'react-router-dom';
-import { Nav } from 'office-ui-fabric-react';
+import React from "react"
+import { withRouter, RouteComponentProps } from "react-router"
+import { Nav } from "office-ui-fabric-react"
 
-export const FabricNav = withRouter(({ history }) => (
-  <Nav
-    onLinkClick={(event, element) => {
-      if (event && element) {
-        event.preventDefault();
-        history.push(element.url);
-      }
-    }}
-    selectedKey="key3"
-    expandButtonAriaLabel="Expand or collapse"
-    selectedAriaLabel="Selected"
-    groups={[
-      {
-        links: [
-          {
-            name: 'Home',
-            url: '/',
-            icon: 'News',
-            key: 'key1',
-          },
-          {
-            name: 'Editor',
-            url: '/editor',
-            icon: 'News',
-            key: 'key2',
-          }
-        ]
-      }
-    ]} />
-));
+interface IProps {
+  className: string
+}
+
+type Props = IProps & RouteComponentProps<{}>
+
+interface IState {
+  readonly selectedKey: string | any
+}
+
+class fabricNav extends React.Component<Props, IState> {
+  constructor(props: Props) {
+    super(props)
+
+    this.state = {
+      selectedKey: "key1",
+    }
+  }
+
+  render() {
+    const { selectedKey } = this.state
+    return (
+      <div className={this.props.className}>
+        <Nav
+          selectedKey={selectedKey}
+          onLinkClick={(event, element) => {
+            if (event && element) {
+              event.preventDefault()
+              this.props.history.push(element.url);
+              this.setState({ ...this.state, selectedKey: element.key })
+            }
+          }}
+          groups={[
+            {
+              links: [
+                {
+                  name: 'Home',
+                  url: '/',
+                  icon: 'News',
+                  key: 'key1',
+                },
+                {
+                  name: 'Editor',
+                  url: '/editor',
+                  icon: 'News',
+                  key: 'key2',
+                }
+              ]
+            }
+          ]}
+        />
+      </div>
+    )
+  }
+}
+
+export const FabricNav = withRouter(fabricNav);
