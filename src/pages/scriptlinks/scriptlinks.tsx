881c37bf67801a857e277a6cedfc2acd636158ec
Tomi Tavela <tomi@Tomis-MacBook-Pro.local>
2019-09-13 23:57:31 +0300

    scriptlinks WIP

--- src/pages/scriptlinks/scriptlinks.tsx
+++ src/pages/scriptlinks/scriptlinks.tsx
@@ -16,188 +16,100 @@ import { CommandBar } from "office-ui-fabric-react/lib/CommandBar";
 import { Stack, TextField } from "office-ui-fabric-react";
 import { IonContent, IonPage, IonGrid, IonRow, IonCol } from "@ionic/react";
 import Header from "../../components/navigation/header";
+import { connect } from "react-redux";
+import { Dispatch } from "redux";
+import { IRootState } from "../../store";
+import { useState } from "react";
 
-const originalItems: IDocument[] = [];
+const originalItems: IScriptLinks[] = [];
 
-export interface IDocument {
+export interface IScriptLinks {
   [key: string]: any;
-  name: string;
-  nameId: number;
+  path: string;
+  sequence: number;
+  scope: string;
 }
 
-interface IAppState {
-  // In filter text box if we type "a" then show only those items which contain "a"
-  filteredItems: IDocument[];
-  // we need to know if the column is to be sorted in ascending or descending
-  detailsListColumns: IColumn[];
-  showItemPanel: boolean;
-  showNewPanel: boolean;
-}
+const ScriptLinks = ({ filteredItems }: HomeProps) => {
+  /* component props */
+  const [showItemPanel, setShowItemPanel] = useState(false);
+  const [showNewPanel, setShowNewPanel] = useState(false);
 
-class ScriptLinks extends React.Component<any, IAppState> {
-  constructor(props: any) {
-    super(props);
-
-    //  Populate filteredItems.
-    if (originalItems.length === 0) {
-      for (let i = 0; i < 10; i++) {
-        originalItems.push({
-          name: "~sitecollection/Style Library/Valo/riot.min.js?v=1.17",
-          nameId: i + 10000,
-          scope: i % 2 == 0 ? "Site Collection" : "Current Web"
-        });
-      }
+  //  Populate filteredItems.
+  if (originalItems.length === 0) {
+    for (let i = 0; i < 10; i++) {
+      originalItems.push({
+        path: "~sitecollection/Style Library/Valo/riot.min.js?v=1.17",
+        sequence: i + 10000,
+        scope: i % 2 == 0 ? "Site Collection" : "Current Web"
+      });
     }
-
-    // Populate columns
-    const detailsListColumns = [
-      {
-        data: "number",
-        fieldName: "nameId",
-        isPadded: true,
-        isResizable: true,
-        isRowHeader: true,
-        isSorted: true,
-        isSortedDescending: false,
-        key: "column2",
-        maxWidth: 100,
-        minWidth: 100,
-        name: "Sequence"
-      },
-      {
-        data: "string",
-        fieldName: "name",
-        isPadded: true,
-        isResizable: true,
-        isRowHeader: true,
-        isSorted: true,
-        isSortedDescending: false,
-        key: "column1",
-        maxWidth: 350,
-        minWidth: 210,
-        name: "Path"
-      },
-      {
-        data: "string",
-        fieldName: "scope",
-        isPadded: true,
-        isResizable: true,
-        isRowHeader: true,
-        isSorted: true,
-        isSortedDescending: false,
-        key: "column3",
-        maxWidth: 350,
-        minWidth: 210,
-        name: "Scope"
-      }
-    ];
-    this.state = {
-      detailsListColumns,
-      filteredItems: originalItems,
-      showItemPanel: false,
-      showNewPanel: false
-    };
   }
 
-  public render(): JSX.Element {
-    const { detailsListColumns, filteredItems } = this.state;
+  // Populate columns
+  const detailsListColumns: IColumn[] = [
+    {
+      data: "number",
+      fieldName: "sequence",
+      isPadded: true,
+      isResizable: true,
+      isRowHeader: true,
+      isSorted: true,
+      isSortedDescending: false,
+      key: "column2",
+      maxWidth: 100,
+      minWidth: 100,
+      name: "Sequence"
+    },
+    {
+      data: "string",
+      fieldName: "path",
+      isPadded: true,
+      isResizable: true,
+      isRowHeader: true,
+      isSorted: true,
+      isSortedDescending: false,
+      key: "column1",
+      maxWidth: 350,
+      minWidth: 210,
+      name: "Path"
+    },
+    {
+      data: "string",
+      fieldName: "scope",
+      isPadded: true,
+      isResizable: true,
+      isRowHeader: true,
+      isSorted: true,
+      isSortedDescending: false,
+      key: "column3",
+      maxWidth: 350,
+      minWidth: 210,
+      name: "Scope"
+    }
+  ];
 
-    return (
-      <>
-        <IonPage>
-          <Header title={"ScriptLinks"} />
-          <IonContent>
-            <IonGrid>
-              <IonRow>
-                <IonCol>
-                  <CommandBar
-                    items={[
-                      {
-                        key: "newItem",
-                        name: "New",
-                        cacheKey: "myCacheKey", // changing this key will invalidate this items cache
-                        iconProps: {
-                          iconName: "Add"
-                        },
-                        ariaLabel: "New",
-                        onClick: this._showNewPanel
-                      }
-                    ]}
-                    overflowButtonProps={{ ariaLabel: "More commands" }}
-                    ariaLabel={
-                      "Use left and right arrow keys to navigate between commands"
-                    }
-                  />
-                  <DetailsList
-                    items={filteredItems}
-                    columns={detailsListColumns}
-                    selectionMode={SelectionMode.single}
-                    setKey="set"
-                    layoutMode={DetailsListLayoutMode.justified}
-                    isHeaderVisible={true}
-                    enterModalSelectionOnTouch={true}
-                    onItemInvoked={this._showItemPanel}
-                  />
-                </IonCol>
-              </IonRow>
-            </IonGrid>
-          </IonContent>
-        </IonPage>
-
-        <Panel
-          isOpen={this.state.showItemPanel}
-          type={PanelType.smallFixedFar}
-          onDismiss={this._hideItemPanel}
-          isLightDismiss={true}
-          isFooterAtBottom={true}
-          headerText="Panel with footer at bottom"
-          closeButtonAriaLabel="Close"
-          onRenderFooterContent={this._onRenderItemFooterContent}
-        />
-        <Panel
-          isOpen={this.state.showNewPanel}
-          type={PanelType.smallFixedFar}
-          onDismiss={this._hideNewPanel}
-          isLightDismiss={true}
-          isFooterAtBottom={true}
-          headerText="Panel with footer at bottom"
-          closeButtonAriaLabel="Close"
-          onRenderFooterContent={this._onRenderNewFooterContent}
-        >
-          <Stack>
-            <TextField
-              label="Custom label rendering"
-              description="Click the (i) icon!"
-            />
-
-            <TextField
-              label="Custom description rendering"
-              description="A colorful description!"
-            />
-          </Stack>
-        </Panel>
-      </>
-    );
-  }
-  private _onRenderItemFooterContent = (): JSX.Element => {
+  const _onRenderItemFooterContent = (): JSX.Element => {
     return (
       <div>
         <PrimaryButton
-          onClick={this._hideItemPanel}
+          onClick={() => setShowItemPanel(false)}
           style={{ marginRight: "8px" }}
         >
           Update
         </PrimaryButton>
-        <DefaultButton onClick={this._hideItemPanel}>Remove</DefaultButton>
+        <DefaultButton onClick={() => setShowItemPanel(false)}>
+          Remove
+        </DefaultButton>
       </div>
     );
   };
 
-  private _onRenderNewFooterContent = (): JSX.Element => {
+  const _onRenderNewFooterContent = (): JSX.Element => {
     return (
       <div>
         <PrimaryButton
-          onClick={this._hideNewPanel}
+          onClick={() => setShowNewPanel(false)}
           style={{ marginRight: "8px" }}
         >
           Add
@@ -206,21 +118,102 @@ class ScriptLinks extends React.Component<any, IAppState> {
     );
   };
 
-  private _showItemPanel = () => {
-    this.setState({ showItemPanel: true });
-  };
+  return (
+    <>
+      <IonPage>
+        <Header title={"ScriptLinks"} />
+        <IonContent>
+          <IonGrid>
+            <IonRow>
+              <IonCol>
+                <CommandBar
+                  items={[
+                    {
+                      key: "newItem",
+                      name: "New",
+                      cacheKey: "myCacheKey", // changing this key will invalidate this items cache
+                      iconProps: {
+                        iconName: "Add"
+                      },
+                      ariaLabel: "New",
+                      onClick: () => setShowNewPanel(true)
+                    }
+                  ]}
+                  overflowButtonProps={{ ariaLabel: "More commands" }}
+                  ariaLabel={
+                    "Use left and right arrow keys to navigate between commands"
+                  }
+                />
+                <DetailsList
+                  items={originalItems}
+                  columns={detailsListColumns}
+                  selectionMode={SelectionMode.none}
+                  setKey="set"
+                  layoutMode={DetailsListLayoutMode.justified}
+                  isHeaderVisible={true}
+                  enterModalSelectionOnTouch={true}
+                  onItemInvoked={() => setShowItemPanel(true)}
+                />
+              </IonCol>
+            </IonRow>
+          </IonGrid>
+        </IonContent>
+      </IonPage>
 
-  private _hideItemPanel = () => {
-    this.setState({ showItemPanel: false });
-  };
+      {/* Panel to item details */}
+      <Panel
+        isOpen={showItemPanel}
+        type={PanelType.smallFixedFar}
+        onDismiss={() => setShowItemPanel(false)}
+        isLightDismiss={true}
+        isFooterAtBottom={true}
+        headerText="Panel with footer at bottom"
+        closeButtonAriaLabel="Close"
+        onRenderFooterContent={_onRenderItemFooterContent}
+      />
 
-  private _showNewPanel = () => {
-    this.setState({ showNewPanel: true });
-  };
+      {/* Panel to create new item */}
+      <Panel
+        isOpen={showNewPanel}
+        type={PanelType.smallFixedFar}
+        onDismiss={() => setShowNewPanel(false)}
+        isLightDismiss={true}
+        isFooterAtBottom={true}
+        headerText="Panel with footer at bottom"
+        closeButtonAriaLabel="Close"
+        onRenderFooterContent={_onRenderNewFooterContent}
+      >
+        {/* Panel form */}
+        <Stack>
+          <TextField
+            label="Custom label rendering"
+            description="Click the (i) icon!"
+          />
 
-  private _hideNewPanel = () => {
-    this.setState({ showNewPanel: false });
-  };
-}
+          <TextField
+            label="Custom description rendering"
+            description="A colorful description!"
+          />
+        </Stack>
+      </Panel>
+    </>
+  );
+};
 
-export default ScriptLinks;
+/* types & redux */
+const mapStateToProps = ({ home }: IRootState) => ({
+  filteredItems: home.list,
+  loading: home.loading
+});
+
+const mapDispatchToProps = (/*dispatch: Dispatch<HomeActions>*/) => ({
+  // addItem: (item: string) => addItemAsync(dispatch, item)
+});
+
+type HomeProps = ReturnType<typeof mapStateToProps> &
+  ReturnType<typeof mapDispatchToProps>;
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(ScriptLinks);
